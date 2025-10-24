df17a4b20f2a46a2ce2a15a105d7ef0e30849ae6
Pouria Ebrahimzade <pouriaebrahimzade@yahoo.com>
2025-10-24 14:26:23 +0330

    Update CI workflows and service configuration for Rebecca project
    - Upgrade Node.js setup to version 20 in build and PR workflows
    - Update Docker actions to latest versions in CI workflows
    - Change image references from 'marzban' to 'rebecca' in docker-compose and service installation script

--- install_service.sh
+++ install_service.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-SERVICE_NAME="marzban"
-SERVICE_DESCRIPTION="Marzban Service"
-SERVICE_DOCUMENTATION="https://github.com/gozargah/marzban"
+SERVICE_NAME="rebecca"
+SERVICE_DESCRIPTION="Rebecca Service"
+SERVICE_DOCUMENTATION="https://github.com/ppouria/rebecca"
 MAIN_PY_PATH="$PWD/main.py"
 SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service"
 
